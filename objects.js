264e1214d62974caf75bbd401cfbbc17af4e62c0
Daniel Williams <danielwil621@gmail.com>
2021-04-02 02:24:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {JCole : "Fire Squad"};
 function updatePlaylist(playlist, artistName, songTitle){
   return playlist[artistName] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName){
+  return delete playlist.artistName;
+}
